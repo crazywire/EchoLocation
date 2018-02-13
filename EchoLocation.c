644ea5a9ced8d6a54186f46b721c5977a16224cd
Abdullahi Muse <35847973+crazywire@users.noreply.github.com>
2018-02-13 15:50:59 -0400

    Update EchoLocation.c

--- EchoLocation.c
+++ EchoLocation.c
@@ -5,6 +5,7 @@
 * Author : Abdul
 */
 
+#include <stdio.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
@@ -13,17 +14,31 @@ volatile int16_t t1=0, t2 =0;
 volatile int16_t triggered =0;
 
 float F_CPU = 14.7546e6, delay = 10e-6, distance;
+int uart_putchar(char c, FILE *stream);
+int uart_getchar(FILE *stream);
+
+FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+FILE mystdin = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
 
 
 
-
-
-int uart_putchar(float distance)
-{
-loop_until_bit_is_set(UCSR0A, UDRE0);
-UDR0 = distance;
-return 0;
+// send a char to PuTTY
+int uart_putchar(char c, FILE *stream) {
+	loop_until_bit_is_set(UCSR0A, UDRE0); // wait until UDRE0 is set (free)
+	UDR0 = c;
+	return 0;
 }
+// get a char from PuTTY
+int uart_getchar(FILE *stream) {
+	loop_until_bit_is_set(UCSR0A, RXC0); // wait until data exists
+	return UDR0; // returns the char read from PuTTY
+}
+void init_uart(void) {
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0); // enables data transfer
+	UBRR0 = 95; // configures a baud rate of 9600
+	stdout = &mystdout; // set the standard out (printf) to PuTTY
+}
+
 
 void generatePulse(){
 PORTB |= (1<<PB1);
@@ -56,11 +71,10 @@ TCCR1B =0; //turn off the timer
 
 int main(void)
 {
+	init_uart();
 	DDRD = 0x00;
 	DDRB = 0xFF;
 
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-	UBRR0 = 95;//7; // configures a baud rate of 115200
 
 	while (1)
 	{
@@ -71,8 +85,13 @@ int main(void)
 	generatePulse();
 	detectEcho();
 	}
+	
+	if (distance) {
+	
+		distance = (double)((t2-t1)*4)/58;
+		printf("distance = %f\n", distance);
+		distance = 0;
+	}
 
-	distance = (double)((t2-t1)*4)/58;
-	uart_putchar(distance);
 	}
 }
