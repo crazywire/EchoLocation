116b9262def4b8c88409f0d938432e535eaaabe0
cmattatall <36604988+cmattatall@users.noreply.github.com>
2018-02-24 18:30:35 -0400

    Update EchoLocation.c
    Updated To use in Midterm conceptual design Report Appendices by Carl Mattatall
    
    Feb 24, 2018.

--- EchoLocation.c
+++ EchoLocation.c
@@ -1,110 +1,137 @@
 /*
-* EchoLocation.c
-*
-* Created: 2018-02-12 10:12:49 PM
-* Author : Abdul
+Echocode
+Author: Carl Mattatall
+Created: Feb 17, 2018
 */
-
 #include <stdio.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 
-volatile int16_t t1=0, t2 =0;
-volatile int16_t triggered =0;
 
-float F_CPU = 14.7546/8, delay = 10, distance=0.0;
+const float fclk = 14.7456e6;
+const float trigger_pulse_length = 10e-6;
+
+
+volatile uint16_t Rising_E_time=0, Falling_E_time=0, time_elapsed;
+
+volatile unsigned short echo_flag = 0;
+
+unsigned int object_distance = 0;
+
+//**** USART FILE IMPORTS AND FUNCTION DECLARATIONS***
 int uart_putchar(char c, FILE *stream);
 int uart_getchar(FILE *stream);
-
 FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 FILE mystdin = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
 
 
 
-// send a char to PuTTY
-int uart_putchar(char c, FILE *stream) {
-	loop_until_bit_is_set(UCSR0A, UDRE0); // wait until UDRE0 is set (free)
+
+
+
+ISR(INT0_vect){
+	printf("The External ISR Works \n ");
+	echo_flag = 1;
+	//EIMSK &= ~(1<<INT0); 
+	//disable future interrupts so that only
+	//the first wire touch will raise the flag
+	//(To prevent bounciness)
+	//We will then re-enable the mask in the main once 
+	//all the code has executed as prompted by this 
+	//external interrupt (INT0)
+}
+
+ISR(TIMER1_COMPA_vect){
+	PORTB &=~(1<<PB1);
+}
+
+ISR(TIMER1_CAPT_vect){
+	//Capture the value of the ICR1 when the rising edge is found
+	//Capture the value of the ICR1 when the falling edge is found
+	if (TCCR1B & (1<<ICES1)){
+		Rising_E_time = ICR1;
+		TCCR1B &= ~(1<<ICES1); //Select the falling edge
+	}else{
+		Falling_E_time = ICR1;
+		TCCR1B |= (1<<ICES1); //Select the rising edge
+	}
+}
+
+void configure_timers(){
+	TCCR1A =0;
+	TCCR1B =(1<<CS11) |(1<<CS10)|(1<<ICES1);
+	//start timer and capture the rising edge
+	TCNT1=0;
+	OCR1A = (int)(fclk*trigger_pulse_length/64); 
+	//Top value for compare ~10us
+}
+
+void configure_ports(){	
+	DDRD = 0;
+	DDRB |= (1<<PB1);
+}
+
+
+void configure_external_interrupts(){
+	EICRA |= (1<<ISC00)|(1<<ISC01);//rising edge only on INT0 (PD2)
+	EIMSK |= (1<<INT0);//enable external interrupt
+	TIMSK1 = (1<<ICIE1)|(1<<OCIE1A); 
+	//enable input capture interrupt & output compare interrupt
+	sei();
+}
+
+int uart_putchar(char c, FILE *stream)
+{
+	loop_until_bit_is_set(UCSR0A, UDRE0);
 	UDR0 = c;
 	return 0;
 }
-// get a char from PuTTY
-int uart_getchar(FILE *stream) {
-	loop_until_bit_is_set(UCSR0A, RXC0); // wait until data exists
-	return UDR0; // returns the char read from PuTTY
-}
-void init_uart(void) {
-	UCSR0B = (1 << RXEN0) | (1 << TXEN0); // enables data transfer
-	UBRR0 = 95; // configures a baud rate of 9600
-	stdout = &mystdout; // set the standard out (printf) to PuTTY
-}
-
-
-void generatePulse(){
-	printf("generate pulse start\n");
-	PORTB |= (1<<PB1);
-	TCCR1A =0;
-	TCCR1B = (1<<CS11); //start timer
-	TCNT1=0;
-	OCR1A = (int)((F_CPU/1600e2)-1);
-	TIFR1 = (1<<OCF1A);
-	while(!(TIFR1 & (1<<OCF1A)));
-	//TIFR1 = (1<<OCF1A);
-	TCCR1B =0;
-	TCNT1=0;
-	PORTB &= ~(1<<PB1);
-	printf("generate pulse end\n");
-}
-
-/*void detectEcho(){
-	
-	printf("detect echo start\n");
-	TCCR1A =0;
-	TCCR1B = (1<<CS11) | (1<<ICES1); //start timer and capture the rising edge
-	TCNT1=0;
-	TIFR1 |= (1<<ICF1);
-	while (!(TIFR1 & (1<<ICF1)));
-	t1 = ICR1L;
-	t1 |= (ICR1H<<8);
-	 //wait for the rising edge
-	 //clear flag
-	
-	printf("detect echo middle\n");
-	TCCR1A =0;
-	TCCR1B =  (1<<CS11); //start timer and capture the rising edge
-	TIFR1 |= (1<<ICF1);
-	while (!(TIFR1 & (1<<ICF1))); //wait for the rising edge
-	printf("detect echo end\n");
-	t2 = ICR1L;
-	t2 |= (ICR1H<<8); //clear flag
-	TCNT1 =0;
-	TCCR1B =0;
-}
-*/
-int main(void)
+int uart_getchar(FILE *stream)
 {
+	/* Wait until data exists. */
+	loop_until_bit_is_set(UCSR0A, RXC0);
+	return UDR0;
+}
+void init_uart(void)
+{
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+	UBRR0 = 95;//7; // configures a baud rate of 115200
+	stdout = &mystdout;
+	stdin = &mystdin;
+	printf("\n USART system booted\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(void){
+	
+	configure_ports();
+	configure_timers();
+	configure_external_interrupts();
 	init_uart();
-	DDRD = 0x00;
-	DDRB = 0xFF;
-
-	printf("starting measurements...\n");
-
-	while (1)
-	{
-		triggered = PIND & (1<<PD7);
-
-		if (triggered)
-		{
-			printf("triggered\n");
-			generatePulse();
-			//detectEcho();
-		}
 		
-		/*distance = (double)((t2-t1)*4)/58;
-		if (distance) {
-			printf("distance = %f\n", distance);
-			distance = 0;
+	
+	while(1){
+		if(echo_flag){
+			echo_flag = 0;
+			PORTB |= (1<<PB1); 
+			// start trigger pulse(ISR turns this pin off);
+			time_elapsed = Falling_E_time-Rising_E_time;
+			object_distance= (int)((time_elapsed*4)/58);
+			printf("The object distance is %d cm\n",object_distance);
 		}
-*/
-	}
+	}				
 }
