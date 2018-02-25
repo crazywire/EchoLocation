de40edeffdbf09bd9e2c7724665ea687617424f9
Abdullahi Muse <35847973+crazywire@users.noreply.github.com>
2018-02-25 09:06:48 -0400

    Update TIMER0_servo.c

--- TIMER0_servo.c
+++ TIMER0_servo.c
@@ -1,6 +1,8 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
+const float fclk = 14.7546e6/1024;
+const float fservo = 60.0;
+const float delta_t = 1.0e-3;
 // initialize PWM
 void pwm_init()
 {
@@ -14,28 +16,28 @@ void pwm_init()
 
 int main(void)
 {
-	uint8_t zero_deg_pulse =7;
-	uint8_t ninety_deg_pulse =19;
-	uint8_t fifteenDegree =2;
+	uint8_t zero_deg_pulse =(int)(fclk*0.49e-3);
+	uint8_t ninety_deg_pulse =(int)(fclk*1.325e-3);
+	uint8_t fifteenDegree =(int)(fclk*0.139e-3);
 	
 	// initialize timer0 in PWM mode
 	pwm_init();
-	
+	OCR0A = (int)(fclk/fservo);
 	while(1)
 	{
 		
-		for (int x = zero_deg_pulse; x <= ninety_deg_pulse; x += fifteenDegree)
+		for (int x = zero_deg_pulse; x < ninety_deg_pulse; x += fifteenDegree)
 		{
 			OCR0A = x;
-		
 			_delay_ms(1000);
+			OCR0A =(int)(fclk/fservo); //top value 
 		}
-		
 		 
 		for ( int x = ninety_deg_pulse; x > zero_deg_pulse; x -= fifteenDegree)
 		{
 			OCR0A =x;
 			_delay_ms(1000);
+			OCR0A =(int)(fclk/fservo); //top value is reset
 		}
 		
 	}
