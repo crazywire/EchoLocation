909c6b083d4c038338586572436cc4ccb0241edc
cmattatall <36604988+cmattatall@users.noreply.github.com>
2018-03-24 17:05:11 -0300

    Update EXTERNAL_SERVO_MOTOR.c

--- EXTERNAL_SERVO_MOTOR.c
+++ EXTERNAL_SERVO_MOTOR.c
@@ -7,7 +7,6 @@
 
 #define F_CPU 14745600 //required definition for <util/delay.h>
 
-
 //libraries
 #include <avr/io.h>
 #include <avr/pgmspace.h>
@@ -33,10 +32,6 @@ ISR(INT2_vect){
 	
 	ext_flag = 1;
 }
-
-
-
-
 //usart function declarations + file pointers
 int uart_putchar(char c, FILE *stream);
 int uart_getchar(FILE *stream);
@@ -64,88 +59,46 @@ void init_uart(void)
 	printf("\nUSART system booted\n");
 }
 
-
 void timer1_servo_cfg(){
-	
 		TCCR1A |= (1<<COM1B1);
 		//on compare match, clear OC1B on upcount, set OC1B on downcount
 		
 		//configure for phase correct pwm TOP at OCR1A
 		TCCR1A |= (1<<WGM10);
 		TCCR1B |= (1<<WGM13);
-		
 		OCR1A = (unsigned int)(fclk_1*servo_period/2.0); //set TOP value
-		
 		TCCR1B |= (1<<CS10); //no prescaler
 		TCNT0 = 0;
 }
 
-
 void interrupt_cfg(){
-	
 	EIMSK |= (1<<INT2); //INT2 = PB2
 	EICRA |= (1<<ISC20); //any logical change triggers ISR
 }
 
 int main(void){
-	
 	init_uart();
 	timer1_servo_cfg();
 	interrupt_cfg();
 	sei();
-	
 	DDRD |= (1<<PD4); //set OC1B to output 
-
-	
-	
 	PORTB &= ~(1<<PB2);
 	DDRB |= (1<<PB2);
-	
-	unsigned short delta_theta = 10; //CHANGE THIS TO CHANGE THE INCREMENT ANGLE
-	
 	const float duty_per_deg = ((max_duty_cycle - min_duty_cycle)/180.0);
-	//how much the duty cycle must increase per additional motor angle
-
+	unsigned short delta_theta = 10; //CHANGE THIS TO CHANGE THE INCREMENT ANGLE
 	unsigned short cnts_per_deg = (unsigned short)(duty_per_deg*OCR1A);
 	unsigned short min_pos_cnts = (unsigned short)(min_duty_cycle*OCR1A);
 	unsigned short max_pos_cnts = (unsigned short)(max_duty_cycle*OCR1A);
 	unsigned short cnts_per_position = (unsigned short)(cnts_per_deg*delta_theta);
-	
 	OCR1B = min_pos_cnts; //initialize motor control to position 0.
-	
-	printf("OCR1B = %u\n", OCR1B);
-	
-
     while (1) 
     {
 		if(ext_flag){
 			ext_flag = 0; //clear external interrupt flag
-			
-			printf("OCR1B = %u\n", OCR1B);
 			OCR1B += cnts_per_position;
-			
-			
 			if(OCR1B > max_pos_cnts){
-				OCR1B = min_pos_cnts;
-				
+				OCR1B = min_pos_cnts;	
 			}
-			
-			
-			
-			
-			
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-    }//end of while
-	
-	
-	
+		}	
+    }//end of while	
 }//end of main
