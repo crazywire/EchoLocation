8761a055661e4d54b9827e8cfcb4d0c28c1ac791
cmattatall <36604988+cmattatall@users.noreply.github.com>
2018-03-25 13:13:51 -0300

    Update Cleaned.c

--- Cleaned.c
+++ Cleaned.c
@@ -22,15 +22,14 @@
 #define fclk_256 57600
 #define fclk_1024 14400
 
+#define spd_sound 34300 //units in cm per sec
+#define distance_factor_8 spd_sound/(2*fclk_8) 
+//distance conversion from clock counts to cm at prescaler of 8
 
-#define min_duty_cyc 0.015 //experimentally determined value
-#define max_duty_cyc 0.0713 //experimentally determined value
-
-
-
-//35 ms is longest time period that will fit in 2*256 cnts at 1024 prescaler.
-
-//PWM pulse lengths (in seconds) for min and max servo motor positions.
+#define servo_period 0.02 //20ms
+#define min_duty_cyc 0.028 //experimentally determined value
+#define max_duty_cyc 0.12 //experimentally determined value
+#define duty_per_degree ((max_duty_cyc-min_duty_cyc)/180)
 
 short motor_angle = -90;
 unsigned int object_distance = 0; //units in cm
@@ -43,23 +42,19 @@ unsigned int object_distance = 0; //units in cm
 
 
 
-//these need to be type int because they measure TCNT1 which can go up to 16 bit.
-volatile unsigned int Rising_Edge_cnt = 0; 
-volatile unsigned int Falling_Edge_cnt = 0;
-//both these variables are used by input capture 
 
 
 
-//usart function declarations + file pointers
+//FUNCTION PROTOTYPES
 int uart_putchar(char c, FILE *stream);
 int uart_getchar(FILE *stream);
 void init_uart(void);
-void timer0_config();
-void timer1_config();
+void timer1_echo_config();
+void timer1_servo_config();
 void timer2_config();
 void ports_config();
 void audio_feedback(unsigned int x);
-void rotate_motor(unsigned short angle, float duty_cycle_per_degree);
+void rotate_motor(unsigned short angle);
 int measure_distance();
 
 FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
@@ -67,14 +62,13 @@ FILE mystdin = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
 
 
 ISR(TIMER1_CAPT_vect){
-	//Capture the value of the ICR1 when the rising edge is found
 	//Capture the value of the ICR1 when the falling edge is found
 	if (TCCR1B & (1<<ICES1)){
-		Rising_Edge_cnt = ICR1;
+		TCNT1 = 0;
+		ICR1 = 0;
 		TCCR1B &= ~(1<<ICES1); //set to falling edge ISR trigger
 	}
 	else{
-		Falling_Edge_cnt = ICR1;
 		TCCR1B |= (1<<ICES1); //set to rising edge ISR trigger
 	}
 }
@@ -83,33 +77,48 @@ ISR(TIMER1_CAPT_vect){
 
 
 
-
-
+//**************************************************************************************************************************************************************
+//***********************MAIN***********************************************************************************************************************************
 int main(void){
 	init_uart();
 	ports_config();
-	timer0_config();
-	timer1_config();
-	timer2_config();
+	timer1_echo_config(); //echo device
+	
+	timer2_config(); //audio feedback from speaker
 	sei();
 	
-	float duty_per_deg = (max_duty_cyc - min_duty_cyc / 180);
+	timer1_servo_config();
 	
 	
+	OCR1B = (unsigned int)(min_duty_cyc*OCR1A); //min position
 	
-	
-	
-	
+
+																										
 	while (1){
-		
+		timer1_echo_config();
 		object_distance = measure_distance();
 //		audio_feedback(object_distance); //3000ms audio pulse
-		rotate_motor(15, duty_per_deg);
+//		printf("distance = %u, angle = %d \n", object_distance, motor_angle);
+		timer1_servo_config(); //set timer1 to servo mode.
+		rotate_motor(15);
+		printf("OCR1B = %u\n", OCR1B);
 		_delay_ms(300);
 		
-		//printf("distance = %u, angle = %d \n", object_distance, motor_angle);
+		
+	
+	
 	}
+	
+	
+																													
+	
+	
+	
 }
+//**************************END OF MAIN***************************
+//***************************************************************
+
+
 
 
 int uart_putchar(char c, FILE *stream)
@@ -134,9 +143,9 @@ void init_uart(void)
 }
 void ports_config(){
 	//****PORTS FOR SERVO MOTOR****
-	DDRB |= (1<<PB4); //set PB4 to output.
-	//PB4 is used as the pwm control signal on the motor.
-	//OC0B = PB4
+	DDRD |= (1<<PD4); 
+	//PD4 is used as the pwm control signal on the motor.
+	//OC1B = PD4
 
 	//*****PORTS FOR ECHOLOCATOR******
 	DDRB |= (1<<PB1); //set PB1 to output 
@@ -152,27 +161,36 @@ void ports_config(){
 	
 	
 }
-void timer0_config(){
-	//TIMER0 IS USED FOR THE SERVO MOTOR
+
+void timer1_servo_config(){
 	
-	TCCR0A |= (1<<COM0B1);//clear OC0B on up-count, set OC0B on down-count
-	TCCR0A |= (1<<WGM00)|(1<<WGM02); //Phase correct pwm with top at OCR0A
-	TCCR0B |= (1<<CS00)|(1<<CS02); //1024 prescaler
-	OCR0A = 255;
-	OCR0B = 3;
-	//divide OCR0A by 2 because phase correct mode.
-	// TCNT0 counts up to TOP (OCR0A in this case)...
-	//and then counts down to BOTTOM (0x00);
-	TCNT0 = 0; //clear timer just in case
+	TCCR1A = (1<<WGM11)|(1<<WGM10)|(1<<COM1B1); 
+	//Phase correct pwm, top at OCR1A
+	//Set OC1B on downcount, clear on upcount
+	
+	
+	TCCR1B = (1<<WGM13)|(1<<CS11); 
+	//phase correct pwm, top at OCR1A
+	//8 prescaler
+	
+	OCR1A = (unsigned int)(fclk_8*servo_period/2);
+	//divide by 2 because TCNT1 in phase correct counts up then counts down
 }
-void timer1_config(){
+
+
+void timer1_echo_config(){
 	//TIMER1 IS USED FOR THE ECHOLOCATOR
 
-	TCCR1B = (1<<CS11)|(1<<CS10); //start TCNT1 counting at 64 prescaler
-	TCCR1B |= (1<<ICES1); //set rising edge trigger mode on the input capture ISR
-	TIMSK1 |= (1<<ICIE1); //enable input capture interrupt
-	TCNT1 = 0; 
+	//64 prescaler
+	//set input capture detection to rising edge
+	//enable input capture interrupt in TIMSK1
+	
+	TCCR1B = (1<<CS11)|(1<<ICES1);
+	TCCR1B |= (1<<ICES1); //rising edge select 
+	TIMSK1 |= (1<<ICIE1); //enble input capture in mask 
+	
 }
+
 void timer2_config(){
 	//TIMER2 IS USED FOR THE AUDIO SPEAKER
 	
@@ -185,11 +203,6 @@ void timer2_config(){
 	//to fclk_128/(2*audio_freq max).
 	//min audio frequency is 291Hz, max is 493Hz
 	//This puts OCR2A ~ [198 ->>117]
-	
-	//OCR2A = (int)(fclk_128/(2*audio_frequency[1]));
-	
-	
-
 }
 void audio_feedback(unsigned int distance){
 	//this function is meant to take in the object distance and play a tone of varying pitch 
@@ -216,35 +229,33 @@ void audio_feedback(unsigned int distance){
 	 _delay_ms(3000);
 	DDRD &= ~(1<<PD7); //disable pwm signal to speaker
 }
+
 int measure_distance(){
 	
 	//****THIS PART TRIGGERS THE SIGNAL TO THE SONIC RANGER****
 	PORTB |= (1<<PB1); //write pin B1 high, wait 10microsec, write PB1 low.
 	_delay_us(10);
 	PORTB &= ~(1<<PB1);
-	TCCR1B |= (1<<ICES1); //set edge detection to rising edge
-	TCNT1 = 0; //clear timer
-	
+	TCCR1B = (1<<ICES1)|(1<<ICNC1)|(1<<CS11); //set edge detection to rising edge
 	//*** INPUT CAPTURE ISR HAPPENS OUT OF FUNCTION, THEN WE RETURN HERE *****
 	
-	return ((int)((Falling_Edge_cnt -Rising_Edge_cnt)*4.4/58));
-	
+	return ((int)((ICR1)*distance_factor_8*1.1)); 
+	//additional factor of 1.1 to account for non-nominal performance
 }
 
 
-void rotate_motor(unsigned short angle, float duty_cycle_per_degree){
+void rotate_motor(unsigned short angle){
 	
-	OCR0B += 1;
-	printf("%u\n", OCR0B);
-	if(OCR0B >= 19){
+	if(OCR1B < (unsigned int)(max_duty_cyc*OCR1A)){
 		
-		OCR0B = 3;
-		motor_angle = -90;
-		printf("\n\n");
+		OCR1B +=(unsigned int)(duty_per_degree*angle*OCR1A);
 	}
 	
-	motor_angle += angle; //increment the global variable (motor_angle) by the passed arguement
+	
+	
+	
+	
+	
+	
+	
 }
-
-
-
